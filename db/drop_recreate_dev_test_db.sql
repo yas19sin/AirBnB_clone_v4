6b6b2a1c95829a8f8a6eac87b31961c9ef35a664
yas19sin <yas19sin@gmail.com>
2025-02-24 07:36:17 +0000

    done

---
+++ db/drop_recreate_dev_test_db.sql
@@ -0,0 +1,13 @@
+-- creates MySQL database hbnb_test_db only if not existing
+-- and gives privileges to user hbnb_test on 2 DB's
+DROP DATABASE IF EXISTS hbnb_test_db;
+DROP DATABASE IF EXISTS hbnb_dev_db;
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
