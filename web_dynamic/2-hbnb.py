d58f05189c23110a00174a4320577d02f163a2a8
Yassine Ennaour <yas19sin@gmail.com>
2025-03-07 05:58:37 +0000

    Update 2-hbnb.py

--- web_dynamic/2-hbnb.py
+++ web_dynamic/2-hbnb.py
@@ -42,6 +42,24 @@ def hbnb_filters(the_id=None):
                            users=users,
                            cache_id=cache_id)
 
+@app.route('/')
+def hbnb_filters1(the_id=None):
+    """
+    handles request to custom template with states, cities & amentities
+    """
+    state_objs = storage.all('State').values()
+    states = dict([state.name, state] for state in state_objs)
+    amens = storage.all('Amenity').values()
+    places = storage.all('Place').values()
+    users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
+                 for user in storage.all('User').values())
+    return render_template('2-hbnb.html',
+                           cache_id=uuid.uuid4(),
+                           states=states,
+                           amens=amens,
+                           places=places,
+                           users=users)
+
 
 if __name__ == "__main__":
     """
